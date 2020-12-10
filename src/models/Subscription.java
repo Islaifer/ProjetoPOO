5b5a6549857e07ab7c4dda7f2af8c625323496d9
islai <islai@LAPTOP-431CGBJI>
2020-12-09 22:06:56 -0300

    Create get into table on SubscriptionDashboardView

--- src/models/Subscription.java
+++ src/models/Subscription.java
@@ -6,6 +6,9 @@ public class Subscription {
 	private int id;
 	private Date dueDate;
 	private SubscriptionStatus status;
+	private String username;
+	private String statusName;
+	private String cpf;
 	private User user;
 	private double amount;
 	
@@ -15,8 +18,35 @@ public class Subscription {
 		this.status = status;
 		this.user = user;
 		this.amount = amount;
+		this.username = this.user.getFirstName() + " " + this.user.getLastName();
+		this.statusName = this.status.getName();
+		this.cpf = this.user.getCpf() + "";
 	}
 	
+	public String getCpf() {
+		String aux = cpf;
+		char [] vet = aux.toCharArray();	
+		if(vet.length == 11) {
+			return aux.substring(0, 3) + "." + aux.substring(3, 6) + "." + aux.substring(6, 9) + "-" + aux.substring(9, 11);
+		}else {
+			return 0 + aux.substring(0, 2) + "." + aux.substring(2, 5) + "." + aux.substring(5, 8) + "-" + aux.substring(8, 10);
+		}
+	}
+
+
+
+	public String getUsername() {
+		return username;
+	}
+
+
+
+	public String getStatusName() {
+		return statusName;
+	}
+
+
+
 	public Subscription(SubscriptionStatus status, User user) {
 		this.status = status;
 		this.user = user;
